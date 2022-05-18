67760ab00c48949ef91e869d999eaa6f7b020f86
marciliodevjava <marciliodevjava@gmail.com>
2022-05-17 21:38:12 -0300

    Sintexe Básica JavaScript

---
+++ aula IV/assets/script.js
@@ -0,0 +1,12 @@
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+//condição IF
+if(jogador1 > 0){
+    console.log('Jogador 1 marcouo ponto!');
+} else if(jogador2 > 0){
+    console.log('Jogador 2 marcouo ponto!');
+} else {
+    console.log('Ninguem marcou ponto!')
+}
