42b408181d26938d120e934995e73087bb7f6638
marciliodevjava <marciliodevjava@gmail.com>
2022-05-18 17:17:28 -0300

    Aula V

--- aula V/assets/script.js
+++ aula V/assets/script.js
@@ -3,3 +3,9 @@ function funcao(){
 }
 
 funcao();
+
+function mensagem(mensagem1, mensagem2){
+    console.log(mensagem1, mensagem2);
+}
+
+mensagem('Tudo certo', "jovem!");
---
+++ aula VI/assets/script.js
@@ -0,0 +1,22 @@
+//Tipos de função
+//declarativa
+function funcao(){
+    console.log('Sou uma função declarativa');
+}
+
+funcao();
+
+//Expresões de funções
+//com nomeação
+var funcaoo = function funcao(){
+    console.log('Função com nomeação!');
+}
+
+funcaoo();
+
+//Arrow function
+var funcaao = () => {
+    console.log('Sou uma arrow function');
+}
+
+funcaao();
